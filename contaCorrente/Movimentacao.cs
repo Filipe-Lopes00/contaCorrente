00ec92727b62bf694393859a5ed7af1e78ef3a92
felip <felip@DESKTOP-0R5TRUD>
2023-04-11 01:06:41 -0300

    Add project files.

---
+++ contaCorrente/Movimentacao.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace contaCorrente
+{
+    internal class Movimentacao
+    {
+        public string descricao;
+        public double valor;
+        public string tipo;
+    }
+}
---
+++ contaCorrente/Program.cs
@@ -0,0 +1,35 @@
+﻿using contaCorrente;
+
+internal class Program
+{    
+          static void Main(string[] args)
+          {
+              ContaCorrente0 conta1 = new ContaCorrente0();
+              conta1.saldo = 1000;
+              conta1.numero = 12;
+              conta1.limite = 0;
+              conta1.ehEspecial = true;
+              conta1.movimentacoes = new Movimentacao[10];
+
+              conta1.Sacar(200);
+
+              conta1.Depositar(300);
+
+              conta1.Depositar(500);
+
+              conta1.Sacar(200);
+
+              ContaCorrente0 conta2 = new ContaCorrente0();
+              conta2.saldo = 300;
+              conta2.numero = 13;
+              conta2.limite = 0;
+              conta2.ehEspecial = true;
+              conta2.movimentacoes = new Movimentacao[10];
+
+              conta1.TransferirPara(conta2, 400);
+
+              conta1.ExibirExtrato();
+
+              Console.ReadKey();
+          }
+}
